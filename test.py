9f4bfa903f5a681d658f88d89654761fafdf6326
Shili88 <taili9736@gmail.com>
2023-12-31 15:21:00 +0000

    testing the code for the wwarehouse_import.py

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
-inventory_file_path = "text/inventory.txt"
+# save and loading (3 functions for each) - balance, inventory (apple;10;5 - each row represent 1 product), operation history (history = keep the sentence - does not have to be translate to anything - review at the warehouse)
+def load_inventory(inventory_file_path="text/inventory.txt"):
+    inventory = {}
+    with open(inventory_file_path) as f:
+        for row in f:
+            name, price, quantity = row.strip().split(";")
+            price = float(price)
+            quantity =float(quantity)
 
-inventory = {}
-with open(inventory_file_path) as f:
-    for row in f:
-        name, quantity, price = row.split(" ; ")
-        quantity = float(price)
-        price = float(quantity)
+            if name in inventory:
+                print(f"Warning: duplicate value of {name}") 
+            
+            inventory[name] = {
+                "price": price,
+                "quantity": quantity,
+            }
+    return inventory
 
-        inventory[name] = {
-            "quantity": quantity,
-            "price": price,
-        }
-
+inventory = load_inventory("text/inventory.txt")
 print(inventory)
